fc4855615b919ffe41b156c24bfc056e62d835b3
sameer kumar singh <sameerkumarsingh@sameers-MacBook-Pro.local>
2021-06-22 21:16:45 +0530

    Finished Gatsby Tut 02

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { container,
+        heading,
+        navLinks,
+        navLinksItem,
+        navLinksText
+} from './layout.module.css'
+
+const Layout = ({ pageTitle, children }) => {
+    return (
+        <main className={container}>
+            <title>{pageTitle}</title>
+            <nav>
+                <ul className={navLinks}>
+                   <li className={navLinksItem}><Link to="/" className={navLinksText}>Home</Link></li>
+                   <li className={navLinksItem}><Link to="/About Me" className={navLinksText}>About</Link></li>
+                </ul>
+            </nav>
+            <h1 className={heading}>{pageTitle}</h1>
+            {children}
+        </main>
+    )
+}
+
+export default Layout
